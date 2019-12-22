cd4a703576bf5cdb5c354bdaea3d2f636abfad09
yjh <1057443070@qq.com>
2019-12-22 21:45:04 +0800

    '个人中心、获取用户信息'

---
+++ src/api/index.js
@@ -0,0 +1,13 @@
+/* 封装的ajax的请求 */
+/*
+* api网站
+* https://developers.weixin.qq.com/miniprogram/dev/api/network/request/wx.request.html
+*
+* */
+const base_url = 'http://localhost:3000'
+
+export default function ajax(url, data={}, type='GET') {
+  return new Promise((resolve, reject) => {
+
+  })
+}
--- src/pages/profile/Profile.vue
+++ src/pages/profile/Profile.vue
@@ -1,14 +1,57 @@
 <template>
-    <div>
-      个人中心
+  <div id="profileContainer">
+    <div class="header">
+      <img src="/static/imgs/personal/personal.png" alt="">
+      <button open-type="getUserInfo" @getuserinfo="handleGetUserInfo">登陆</button>
     </div>
+    <div class="cardList">
+      <div class="card">
+        <span>扫码看书</span>
+        <span class="more"> > </span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-  export default {}
+  export default {
+    methods: {
+      handleGetUserInfo(res){
+        console.log(res)
+      },
+    }
+  }
 </script>
 
 
-<style>
-
+<style lang="stylus" rel="stylesheet/stylus">
+  #profileContainer
+    .header
+      padding 40rpx
+      background #489B99
+      img
+        width 100rpx
+        height 100rpx
+        vertical-align middle
+        /* 将头像设置为圆形   */
+        border-radius 50rpx
+      button
+        display inline-block
+        height 60rpx
+        line-height 60rpx
+        background rgba(255,255,255, 0.5)
+        margin-left 40rpx
+        // 垂直居中
+        vertical-align middle
+        max-width 200rpx
+    .cardList
+      .card
+        width 94%
+        height 60rpx
+        line-height 60rpx
+        margin 10rpx auto
+        border-bottom 1rpx solid #eee
+        padding 10rpx
+        .more
+          float right
 </style>
