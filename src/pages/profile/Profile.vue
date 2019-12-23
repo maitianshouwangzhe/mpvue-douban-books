5a90fbe7adc199d6f9672b6351efbf8cb6ecab68
yujihua <yujh85@chinaunicom.cn>
2019-12-23 10:24:50 +0800

    '授权登录、再次刷新仍显示用户信息'

--- src/pages/profile/Profile.vue
+++ src/pages/profile/Profile.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="profileContainer">
     <div class="header">
-      <img src="/static/imgs/personal/personal.png" alt="">
-      <button open-type="getUserInfo" @getuserinfo="handleGetUserInfo">登陆</button>
+      <img :src=" userInfo.avatarUrl ? userInfo.avatarUrl : '/static/imgs/personal/personal.png' " alt="">
+      <button open-type="getUserInfo" @getuserinfo="handleGetUserInfo">{{ userInfo.nickName ? userInfo.nickName : '登录'}}</button>
     </div>
     <div class="cardList">
-      <div class="card">
+      <div class="card" @click="toScanCode">
         <span>扫码看书</span>
         <span class="more"> > </span>
       </div>
@@ -15,11 +15,44 @@
 
 <script>
   export default {
-    methods: {
-      handleGetUserInfo(res){
-        console.log(res)
+      data(){
+          return {
+              userInfo: {},
+          }
       },
-    }
+
+      mounted(){
+          // 调用前需要用户授权, 即：第一次不执行，
+          // 在获得授权之后，以后的刷新或者再次进入小程序后，会自动执行
+          wx.getUserInfo({
+              success: (res) => {
+                  console.log('mouted里面的数据',res)
+                  // 更新userInfo的状态数据
+                  this.userInfo = res.userInfo
+              },
+              fail: () => {
+                  console.log('没有授权，获取失败');
+              }
+          })
+      },
+
+      methods: {
+          handleGetUserInfo(res){
+          console.log(res)
+          if (res.mp.detail.userInfo){
+              // 更新状态
+              this.userInfo = res.mp.detail.userInfo
+          }},
+          // 打开摄像头扫码
+          toScanCode(){
+              wx.scanCode({
+                  scanType: ['barCode', 'qrCode', 'datamatrix', 'pdf417'],
+                  success: (res)=>{
+                      console.log('扫码结果', res)
+                  },
+              })
+          }
+      },
   }
 </script>
 
